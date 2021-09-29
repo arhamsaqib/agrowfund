92efb6ec80e61388bd0adf3e6323a079b945ce50
Arham Saqib <arhamsaqib@outlook.com>
2021-09-29 07:46:21 +0500

    Bottom Tab Icons

--- app/screens/homeBottomNav/BottomNav.tsx
+++ app/screens/homeBottomNav/BottomNav.tsx
@@ -4,16 +4,60 @@ import {HomeStack} from './home/homeStack';
 import {ProjectsStack} from './projects/projectsStack';
 import {HistoryStack} from './history/historyStack';
 import {SettingsStack} from './settings/settingsStack';
+import {COLORS} from '../../colors';
+import Icon from 'react-native-vector-icons/Ionicons';
+import FA from 'react-native-vector-icons/Fontisto';
 
 const BTabs = createBottomTabNavigator();
 
 export const BottomTabs = () => {
   return (
-    <BTabs.Navigator screenOptions={{headerShown: false}}>
-      <BTabs.Screen name="Home" component={HomeStack} />
-      <BTabs.Screen name="Projects" component={ProjectsStack} />
-      <BTabs.Screen name="History" component={HistoryStack} />
-      <BTabs.Screen name="Settings" component={SettingsStack} />
+    <BTabs.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: COLORS.primary_green,
+        tabBarInactiveTintColor: '#666666',
+        tabBarLabelStyle: {
+          fontFamily: 'Poppins-Medium',
+          fontSize: 12,
+        },
+      }}>
+      <BTabs.Screen
+        name="Home"
+        component={HomeStack}
+        options={{
+          tabBarIcon: ({color}) => (
+            <Icon name="home-outline" color={color} size={25} />
+          ),
+        }}
+      />
+      <BTabs.Screen
+        name="Projects"
+        component={ProjectsStack}
+        options={{
+          tabBarIcon: ({color}) => (
+            <Icon name="grid-outline" color={color} size={25} />
+          ),
+        }}
+      />
+      <BTabs.Screen
+        name="History"
+        component={HistoryStack}
+        options={{
+          tabBarIcon: ({color}) => (
+            <FA name="history" color={color} size={25} />
+          ),
+        }}
+      />
+      <BTabs.Screen
+        name="Settings"
+        component={SettingsStack}
+        options={{
+          tabBarIcon: ({color}) => (
+            <Icon name="settings-outline" color={color} size={25} />
+          ),
+        }}
+      />
     </BTabs.Navigator>
   );
 };
